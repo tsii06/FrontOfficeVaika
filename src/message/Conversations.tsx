85ea77feda2dd7910c037b5bcbf3425c0732c312
Antonio001931 <andriamihavanaantonio@gmail.com>
2024-02-09 23:46:43 +0100

    uu

--- src/message/Conversations.tsx
+++ src/message/Conversations.tsx
@@ -2,21 +2,7 @@ import Header from '../components/Header';
 import ConversationList from './ConversationList';
 import './conversation.css';
 const Conversations: React.FC = () => {
-    const conversations = [
-        {
-          id: 1,
-          name: 'Ami 1',
-          lastMessage: 'Salut, ça va ?',
-          unreadCount: 2,
-        },
-        {
-          id: 2,
-          name: 'Ami 2',
-          lastMessage: 'Coucou !',
-          unreadCount: 0,
-        },
-        // Ajoutez d'autres conversations ici
-      ];
+
   
     return (
         <div>
